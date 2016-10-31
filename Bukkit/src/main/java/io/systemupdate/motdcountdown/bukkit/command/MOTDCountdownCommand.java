df8c002dffd0588dd0d61d32697e7d4037387a1e
SystemUpdate <contact@systemupdate.io>
2016-10-31 20:05:58 +0000

    Update LICENSE

--- Bukkit/src/main/java/io/systemupdate/motdcountdown/bukkit/command/MOTDCountdownCommand.java
+++ Bukkit/src/main/java/io/systemupdate/motdcountdown/bukkit/command/MOTDCountdownCommand.java
@@ -1,21 +1,19 @@
 /*
- *   COPYRIGHT NOTICE
+ *     Simple plugin to allow countdowns in MOTD. Works with BungeeCord or Bukkit.
+ *     Copyright (C) 2016  SystemUpdate
  *
- *   Copyright (C) 2016, SystemUpdate, <admin@systemupdate.io>.
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
  *
- *   All rights reserved.
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
  *
- *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF THIRD PARTY RIGHTS. IN
- *   NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
- *   DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
- *   OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
- *   OR OTHER DEALINGS IN THE SOFTWARE.
- *
- *   Except as contained in this notice, the name of a copyright holder shall not
- *   be used in advertising or otherwise to promote the sale, use or other dealings
- *   in this Software without prior written authorization of the copyright holder.
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package io.systemupdate.motdcountdown.bukkit.command;
